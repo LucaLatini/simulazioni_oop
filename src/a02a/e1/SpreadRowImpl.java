f609d01126cc70a8f19fc46b2515e77d92cd433f
Luca Latini <luca.latini2@studio.unibo.it>
2025-12-19 15:08:22 +0100

    risolto esercizio 1 a02a

--- src/a02a/e1/SpreadRowImpl.java
+++ src/a02a/e1/SpreadRowImpl.java
@@ -1,61 +1,132 @@
 package a02a.e1;
 
+import java.lang.foreign.Linker.Option;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.Set;
+import java.util.function.*;
 
 public class SpreadRowImpl implements SpreadRow {
 
-    public SpreadRowImpl(int i) {
+    private interface Cell {
+        Optional<Integer> calculate(List<Optional<Integer>> rowValues);
+    }
 
+    // 1. Cella Vuota (Semplice)
+    private class EmptyCell implements Cell {
+        @Override
+        public Optional<Integer> calculate(List<Optional<Integer>> rowValues) {
+            return Optional.empty();
+        }
+    }
+
+    // 2. Cella Numero (Semplice)
+    private class NumberCell implements Cell {
+        private final int value;
+
+        public NumberCell(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public Optional<Integer> calculate(List<Optional<Integer>> rowValues) {
+            return Optional.of(value);
+        }
+    }
+
+    // 3. LA SVOLTA: Una sola classe Formula per tutto!
+    // Questa classe accetta una "funzione" (la strategia) nel costruttore.
+    private class GenericFormula implements Cell {
+        // Usa una Function che prende la lista dei risultati e restituisce il risultato
+        private final java.util.function.Function<List<Optional<Integer>>, Optional<Integer>> function;
+
+        public GenericFormula(Function<List<Optional<Integer>>, Optional<Integer>> function) {
+            this.function = function;
+        }
+
+        @Override
+        public Optional<Integer> calculate(List<Optional<Integer>> rowValues) {
+            // Delega il calcolo alla funzione che gli abbiamo passato
+            return function.apply(rowValues);
+        }
+    }
+
+    private List<Cell> cells = new LinkedList<>();;
+
+    public SpreadRowImpl(int i) {
+        for (int index = 0; index < i; index++) {
+            this.cells.add(new EmptyCell());
+        }
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        return this.cells.size();
     }
 
     @Override
     public boolean isFormula(int index) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isFormula'");
+        return this.cells.get(index) instanceof GenericFormula;
     }
 
     @Override
     public boolean isNumber(int index) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isNumber'");
+        return this.cells.get(index) instanceof NumberCell;
     }
 
     @Override
     public boolean isEmpty(int index) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+        return this.cells.get(index) instanceof EmptyCell;
     }
 
     @Override
     public List<Optional<Integer>> computeValues() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'computeValues'");
+        List<Optional<Integer>> result = new ArrayList<>();
+        for (Cell cell : this.cells) {
+            Optional<Integer> val = cell.calculate(result);
+            result.add(val);
+
+        }
+        return result;
     }
 
     @Override
     public void putNumber(int index, int number) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'putNumber'");
+        this.cells.set(index, new NumberCell(number));
     }
 
     @Override
     public void putSumOfTwoFormula(int resultIndex, int index1, int index2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'putSumOfTwoFormula'");
+
+        this.cells.set(resultIndex, new GenericFormula(r -> {
+            var n1 = r.get(index1);
+            var n2 = r.get(index2);
+            if (n1.isPresent() && n2.isPresent()) {
+                return Optional.of(n1.get() + n2.get());
+            } else {
+                return Optional.empty();
+            }
+
+        }));
     }
 
     @Override
     public void putMultiplyElementsFormula(int resultIndex, Set<Integer> indexes) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'putMultiplyElementsFormula'");
+
+        this.cells.set(resultIndex, new GenericFormula( r -> {
+            int moltiplication = 1;
+            for (Integer index : indexes) {
+                if(r.get(index).isPresent()){
+                    moltiplication = moltiplication * (r.get(index).get());
+                }else{
+                    moltiplication = 0;
+                }
+            }
+            return Optional.of(moltiplication);
+        }));
     }
 
 }
